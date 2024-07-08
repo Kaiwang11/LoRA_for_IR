2b86fb67ea2920723a4b14d8e75f139d06596599
Kaiwang11 <bear6548@gmail.com>
2024-07-09 03:35:24 +0800

    align main and Loratransformer  with local

--- sentence_transformers/models/LoraTransformer.py
+++ sentence_transformers/models/LoraTransformer.py
@@ -5,6 +5,12 @@ from typing import List, Dict, Optional, Union, Tuple
 import os
 from peft import LoraConfig, get_peft_model,AdaLoraConfig
 import torch
+import os
+import math
+from peft import AdaLoraModel
+import logging
+import time
+logger = logging.getLogger(__name__)
 class LoraTransformer(nn.Module):
     """Huggingface AutoModel to generate token embeddings.
     Loads the correct class, e.g. BERT / RoBERTa etc.
@@ -21,18 +27,18 @@ class LoraTransformer(nn.Module):
                  model_args: Dict = {}, cache_dir: Optional[str] = None,
                  tokenizer_args: Dict = {}, do_lower_case: bool = False,
                  tokenizer_name_or_path : str = None,
-                 lora_config:Dict={},dora: bool=False,vera:bool=False ):
+                 lora_config:Dict={} ,experiment_type:str=None):
         
 
         super(LoraTransformer, self).__init__()
         self.config_keys = ['max_seq_length', 'do_lower_case']
         self.do_lower_case = do_lower_case
-        self.dora=dora
-        self.vera=vera
-        config = AutoConfig.from_pretrained(model_name_or_path, **model_args, cache_dir=cache_dir)
+        # self.num_layers=config['num_']
+        config = AutoConfig.from_pretrained(model_name_or_path, **model_args, cache_dir=cache_dir,output_hidden_states=True)
         self._load_model(model_name_or_path, config,lora_config, cache_dir)
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path if tokenizer_name_or_path is not None else model_name_or_path, cache_dir=cache_dir, **tokenizer_args)
+        self.num_hidden_layers=config.num_hidden_layers
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path if tokenizer_name_or_path is not None else model_name_or_path, cache_dir=cache_dir, **tokenizer_args,pad_token='[PAD]')
+        self.exp_type=experiment_type
 
         #No max_seq_length set. Try to infer from model
         if max_seq_length is None:
@@ -55,28 +61,29 @@ class LoraTransformer(nn.Module):
             #     # bnb_4bit_compute_dtype=torch.bfloat16,
             #     bnb_4bit_use_double_quant=True
             # )
-            model = AutoModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
+            model = AutoModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir,device_map='cuda')
             # model = AutoModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir, quantization_config=quantization_config)
             print("Original model")
-            self.print_trainable_parameters(model)
+            ori_name=self.print_trainable_parameters(model)
 
             self.auto_model=get_peft_model(model,lora_config)
             print("Lora model")
             self.print_trainable_parameters(self.auto_model)
-    
+            
+            # import IPython;IPython.embed(colors='linux');exit(1)
     def print_trainable_parameters(self,model):
-        """
-        Prints the number of trainable parameters in the model.
-        """
         trainable_params = 0
         all_param = 0
-        for _, param in model.named_parameters():
+        trainable_name=[]
+        for name, param in model.named_parameters():
             all_param += param.numel()
             if param.requires_grad:
+                trainable_name.append(name)
                 trainable_params += param.numel()
         print(
             f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
         )
+        return trainable_name
 
     def _load_t5_model(self, model_name_or_path, config, cache_dir):
         """Loads the encoder model from T5"""
@@ -85,31 +92,80 @@ class LoraTransformer(nn.Module):
         self.auto_model = T5EncoderModel.from_pretrained(model_name_or_path, config=config, cache_dir=cache_dir)
 
     def __repr__(self):
-        return "Transformer({}) with Transformer model: {} ".format(self.get_config_dict(), self.auto_model.__class__.__name__)
+        return "LoraTransformer({}) with Transformer model: {} ".format(self.get_config_dict(), self.auto_model.__class__.__name__)
 
     def forward(self, features):
         """Returns token_embeddings, cls_token"""
         trans_features = {'input_ids': features['input_ids'], 'attention_mask': features['attention_mask']}
         if 'token_type_ids' in features:
             trans_features['token_type_ids'] = features['token_type_ids']
-        if self.dora==True:
-            modules=[name for name,param in self.auto_model.named_parameters()  if param.requires_grad and 'lora_magnitude_vector' in name and  str(self.num_hidden_layers-1) in name]
+        # B_weight=self.auto_model.get_submodule('base_model.model.transformer.layer.5.attention.out_lin.lora_B.default').weight.mean(dim=1)
+        # B_vlin_weight=self.auto_model.get_submodule('base_model.model.transformer.layer.5.attention.v_lin.lora_B.default').weight.mean(dim=1)
+        if self.exp_type=='single':
+            modules=[name for name,param in self.auto_model.named_parameters() if param.requires_grad and 'lora_A' in name and str(self.num_hidden_layers-1) in name]
+            # A_mag_weight=self.auto_model.get_submodule(modules[-1][:-8]).default
+            # A_qlin_weight=self.auto_model.get_submodule(modules[0][:-7]).weight
+            # A_klin_weight=self.auto_model.get_submodule(modules[1][:-7]).weight
+            # A_vlin_weight=self.auto_model.get_submodule(modules[2][:-7]).weight
+            A_total_weight=self.auto_model.get_submodule(modules[-2][:-7]).weight
+            features.update({'last_lora':A_total_weight.mean(dim=0)})
+        elif self.exp_type=='multi':
+            soft=nn.Softmax(1)
+            weight=soft(torch.matmul(A_qlin_weight.transpose(1,0),A_klin_weight/math.sqrt(384)))*A_vlin_weight.mean(dim=0)
+            features.update({'last_lora':weight})
+        # modules=[name for name,param in self.auto_model.named_parameters() if param.requires_grad and 'lora_A' in name]
+        # filename='/tmp2/ikwang/beir/examples/retrieval/training/ranks.csv'
+        # for mod in modules :
+        #     w=self.auto_model.get_submodule(mod[:-8]).default
+        #     U, S, V = torch.svd(w)
+        #     rank = torch.sum(S > 1e-10).item()
+        #     # import IPython;IPython.embed(colors='linux');exit(1)
+        #
+        #     if os.path.exists(filename):
+        #         file=open(filename,'a')
+        #         file.writelines(mod[29:-7]+','+str(rank)+'\n')
+        #     else:
+        #
+        #         file=open(filename,'w')
+        #         file.writelines(mod[29:-7]+','+str(rank)+'\n')
+        # import IPython;IPython.embed(colors='linux');exit(1)
+        #
+        # modules=[name for name,param in self.auto_model.named_parameters() if param.requires_grad and 'lora_magnitude_vector' in name and str(self.num_hidden_layers-1) in name]
+        # import IPython;IPython.embed(colors='linux');exit(1)
+        # A_total_weight=torch.sum(torch.stack([self.auto_model.get_submodule(module[:-7]).weight.mean(dim=0) for module in modules]),dim=0)
 
-            mag_weight=self.auto_model.get_submodule(modules[-1][:-8]).default
-        elif self.vera==True:
-            modules=[name for name,param in self.auto_model.named_parameters()  if param.requires_grad and 'vera_lambda_b' in name and  str(self.num_hidden_layers-1) in name]
 
+        # import IPython;IPython.embed(colors='linux');exit(1)
+        # A_weight=self.auto_model.get_submodule('base_model.model.transformer.layer.5.attention.out_lin.lora_A.default').weight#.mean(dim=0)
+        
+        # A_qlin_weight=self.auto_model.get_submodule('base_model.model.encoder.layer.11.attention.self.query.lora_A.default').weight
+        # A_klin_weight=self.auto_model.get_submodule('base_model.model.encoder.layer.11.attention.self.key.lora_A.default').weight
+        # A_vlin_weight=self.auto_model.get_submodule('base_model.model.encoder.layer.11.attention.self.value.lora_A.default').weight
+        # A_weight=self.auto_model.get_submodule('base_model.model.transformer.layer.5.attention.out_lin.lora_A.default').weight#.mean(dim=0)
+        #a max_ = torch.max(A_weight, 1)[0]
+
+        # A_weight=self.auto_model.get_submodule('base_model.model.encoder.layer.11.attention.self.value.lora_A.default').weight.mean(dim=0)
+        
+        # logger.info("Making embedding")
+        if os.path.exists('/tmp2/ikwang/beir/examples/retrieval/training/full_emb.txt'):
+            file=open('/tmp2/ikwang/beir/examples/retrieval/training/full_emb.txt','a')
         else:
-            modules=[name for name,param in self.auto_model.named_parameters() if param.requires_grad and   'lora_A' in name and str(self.num_hidden_layers-1) in name]
-
-        A_qlin_weight=self.auto_model.get_submodule(modules[0][:-7]).weight
-        A_klin_weight=self.auto_model.get_submodule(modules[1][:-7]).weight
-        A_vlin_weight=self.auto_model.get_submodule(modules[2][:-7]).weight
-        A_outlin_weight=self.auto_model.get_submodule(modules[3][:-7]).weight
-
-        if A_qlin_weight.sum()==0:
-            A_qlin_weight=torch.ones_like(A_qlin_weight)
+            file=open('/tmp2/ikwang/beir/examples/retrieval/training/full_emb.txt','w')     
+        s1=time.time()
         output_states = self.auto_model(**trans_features, return_dict=False)
+        s2=time.time()
+        file.write(str(s2-s1)+'\n')
+        # lora_E=self.auto_model.get_submodule('base_model.model.transformer.layer.5.attention.out_lin.lora_E').default
+        # print('ranks ', torch.linalg.matrix_rank(lora_E))
+        # print('ranks ', lora_E.shape)
+        # if B_weight.sum()==0:
+        #     features.update({'last_lora':A_weight})
+        # else:
+        #     features.update({'last_lora':A_weight*B_weight})        
+        # features.update({'last_lora':A_total_weight.mean(dim=0)})
+        # linear=nn.Linear(32,1).to('cuda')
+        # weight=linear(A_qlin_weight.T).mean(dim=0)
+
         output_tokens = output_states[0]
         features.update({'token_embeddings': output_tokens, 'attention_mask': features['attention_mask']})
 
@@ -120,6 +176,7 @@ class LoraTransformer(nn.Module):
 
             hidden_states = output_states[all_layer_idx]
             features.update({'all_layer_embeddings': hidden_states})
+        # import IPython;IPython.embed(colors='linux');exit(1)
         return features
 
     def get_word_embedding_dimension(self) -> int:
--- src/main.py
+++ src/main.py
@@ -4,49 +4,64 @@ from beir.losses import BPRLoss,MarginMSELoss
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.train import TrainRetriever
 import pathlib, os
-
+import json
 import logging
 from time import time
 import tracemalloc
 import argparse
 # from peft.tuners.dora import DoraConfig
-from peft import LoraConfig,VeraConfig
+# from peft.tuners.rosa import RosaConfig, RosaSchedulerT,
+from peft import LoraConfig,LoftQConfig,get_peft_model,AdaLoraConfig,IA3Config,LoHaConfig,LoKrConfig
 import torch
 from lora_plus import create_loraplus_optimizer
 # loftq_config = LoftQConfig(loftq_bits=4)
+# if not  os.path.exists(f"./full_backward.txt"):
+#     file_b=open(f"./full_backward.txt",'w')
+# else:
+#     file_b=open(f"./full_backward.txt",'a')
+# if not  os.path.exists(f"./full_optimize.txt"):
+#     file_o=open(f"./full_optimize.txt",'w')
+# else:
+#     file_o=open(f"./full_optimize.txt",'a')
+
 parser = argparse.ArgumentParser(description='Argument Parser for Dataset, Experiment Name, and Epoch Number')
 parser.add_argument('-d', '--dataset', type=str, help='Name of the dataset',default='nfcorpus')
-parser.add_argument('-exp', '--experiment', type=str, help='Name of the experiment',default='')
+parser.add_argument('-exp_name', '--experiment_name', type=str, help='Name of the experiment',default='')
+parser.add_argument('-exp_type', '--experiment_type', type=str, help='type of the experiment such as single,multi,None',default='None')
 parser.add_argument('-epoch', '--epoch', type=int, help='Number of epochs',default=10)
 parser.add_argument('-p','--plus',type=bool,help = 'Lora plus',default=False)
 parser.add_argument('-r','--rank',type=int,help='rank',default=32)
 parser.add_argument('-pool','--pool_mode',type=str,help='pooling_mode',default=None)
-parser.add_argument('-dora','--dora',type=bool,help='execute dora,default False',default=False )
 
-parser.add_argument('-vera','--vera',type=bool,help='execute vera,default False',default=False )
+parser.add_argument('-model','--model_name',type=str,help='hugging face model name',default="distilbert-base-uncased")
+parser.add_argument('-dora','--dora',type=bool,help='dora',default=False)
+parser.add_argument('-vera','--vera',type=bool,help='vera',default=False)
+parser.add_argument('-batch','--batch_size',type=int,help='batch zise',default=32)
+parser.add_argument('-target_module','--target_module',type=str,help='target module',default='["q_lin","out_lin","k_lin","v_lin"]')
+# parser.add_argument('-pool','--pool_mode',type=str,help='pooling_mode',default=None)
 args = parser.parse_args()
-# locals().update(vars(args))6  
-
 if args.vera==True:
 
     lora_config =VeraConfig(
-        r=args.rank,
-        target_modules=["q_lin","v_lin","k_lin","out_lin"],#['query','value','key'],#["q_lin","v_lin","k_lin","out_lin"],#,###,
-        bias="all", #‘none’, ‘all’ or ‘lora_only'
-        modules_to_save=["decode_head"],
-    )
+                r=args.rank,
+                target_modules=json.loads(args.target_module),
+                bias="all", 
+                modules_to_save=["decode_head"],
+            )
 else:
     lora_config = LoraConfig(
-        r=args.rank,
+         r=args.rank,
         lora_alpha=args.rank*2,
-        target_modules=["q_lin","v_lin","k_lin","out_lin"],#,
+        target_modules=json.loads(args.target_module),
         lora_dropout=0.5,
         bias="all", #‘none’, ‘all’ or ‘lora_only'
         modules_to_save=["decode_head"],
-        # loftq_config=loftq_config
         use_dora=args.dora
     )
-
+# lora_config = AdaLoraConfig(
+#     peft_type="ADALORA", target_r=16, init_r=16,lora_alpha=64, target_modules=["q_lin","v_lin","k_lin","out_lin"],
+#     lora_dropout=0.01,
+# )
 start=time()
 logging.basicConfig(format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S',level=logging.INFO,handlers=[LoggingHandler()])
 
@@ -64,22 +79,26 @@ elif dataset=='scifact':
 
 elif dataset=='fiqa':
     data_path_train,data_path_test='./datasets/fiqa','./datasets/fiqa'
+elif dataset=='hotpotqa':
+    data_path_train,data_path_test='./datasets/hotpotqa','./datasets/hotpotqa'
 else :
     data_path_train='./datasets/nfcorpus'
     data_path_test='./datasets/nfcorpus'
 
-exp=parser.parse_args().experiment
 corpus, queries, qrels = GenericDataLoader(data_path_train).load(split="train")
 dev_corpus, dev_queries, dev_qrels = GenericDataLoader(data_path_test).load(split="test")
 
 tracemalloc.start()
-model_name ="distilbert-base-uncased"
-
-word_embedding_model = models.LoraTransformer(model_name,lora_config=lora_config, max_seq_length=350,dora=args.dora,vera=args.vera)
+model_name=args.model_name #"hkunlp/instructor-xl"#distilbert-base-uncased"#"intfloat/e5-small"##"# "BAAI/bge-small-en"#v#
+word_embedding_model = models.LoraTransformer(model_name,lora_config=lora_config, max_seq_length=350,experiment_type=args.experiment_type)
+# word_embedding_model = models.PeftTransformer(model_name,peft_config=lora_config, max_seq_length=350)
 pooling_model = models.Pooling(pooling_mode=args.pool_mode,word_embedding_dimension=word_embedding_model.get_word_embedding_dimension())
+
+
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+# model = SentenceTransformer(modules=[word_embedding_model])
 # snap = tracemalloc.take_snapshot()
-retriever = TrainRetriever(model=model, batch_size=32)
+retriever = TrainRetriever(model=model, batch_size=args.batch_size)
 #### Prepare training samples
 train_samples = retriever.load_train(corpus, queries, qrels)
 train_dataloader = retriever.prepare_train(train_samples, shuffle=True)
@@ -93,7 +112,7 @@ train_loss = losses.MultipleNegativesRankingLoss(model=retriever.model)
 #### Prepare dev evaluato
 ir_evaluator = retriever.load_ir_evaluator(dev_corpus, dev_queries, dev_qrels)
 
-model_save_path = os.path.join(pathlib.Path(__file__).parent.absolute(), f"output/lora_{exp}_epoch{num_epochs}", "{}-v1-{}".format(model_name, dataset))
+model_save_path = os.path.join(pathlib.Path(__file__).parent.absolute(), f"output/lora_{args.experiment_name}_epoch{num_epochs}", "{}-v1-{}".format(model_name, dataset))
 
 os.makedirs(model_save_path, exist_ok=True)
 
@@ -102,12 +121,12 @@ evaluation_steps = 100000
 warmup_steps = int(len(train_samples) * num_epochs / retriever.batch_size * 0.1)
 optimizer=None
 
-if parser.parse_args().plus==True:
+if args.plus==True:
     optimizer_cls = torch.optim.AdamW
     optimizer_kwargs = {'lr': 2e-5, 'eps': 1e-6, 'betas': (0.9, 0.999), 'weight_decay': 0.0}
     loraplus_lr_ratio = 20.0
     optimizer = create_loraplus_optimizer(model, optimizer_cls, optimizer_kwargs, loraplus_lr_ratio)
-
+snapshot1 = tracemalloc.take_snapshot()
 retriever.fit(train_objectives=[(train_dataloader, train_loss)],
                 evaluator=ir_evaluator,
                 epochs=num_epochs,
@@ -119,7 +138,12 @@ retriever.fit(train_objectives=[(train_dataloader, train_loss)],
              optimizer_custom=optimizer
              )
 end=time()
+snapshot2 = tracemalloc.take_snapshot()
+stats = snapshot2.statistics('filename')
 current, peak = tracemalloc.get_traced_memory()
+top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+# for stat in top_stats[:10]:  # Show top 10 memory consuming lines
+#     print(stat)
 # Output the total memory usage
 print(f"Total memory usage: {current / 10**6} MB")
 tracemalloc.stop()
