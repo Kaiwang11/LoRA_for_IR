1608e34639b1e059a3ae606fcbea147e73e582e8
Kaiwang11 <bear6548@gmail.com>
2024-07-22 13:48:19 +0800

    add arguments on data_load  and  README

--- data_load.py
+++ data_load.py
@@ -5,8 +5,11 @@ from beir.retrieval.train import TrainRetriever
 import pathlib, os, gzip
 import logging
 import json
+import argparse
 
-dataset = "nfcorpus"
+parser=argparse.AgumentParser(description='Download datase . I experimnt nq,nfcorpus,fiqa,scifact .' )
+parser.add_argument('-d','--dataset', type=str,help='dataset name',default='fiqa')
+dataset = "fiqa"
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "./datasets")
 util.download_and_unzip(url, out_dir)
