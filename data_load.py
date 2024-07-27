c395f6891173e6eb1417e254cd404a094bf097a0
Kaiwang11 <bear6548@gmail.com>
2024-07-27 13:23:49 +0800

    update data_load

--- data_load.py
+++ data_load.py
@@ -7,9 +7,9 @@ import logging
 import json
 import argparse
 
-parser=argparse.AgumentParser(description='Download datase . I experimnt nq,nfcorpus,fiqa,scifact .' )
+parser=argparse.ArgumentParser(description='Download datase . I experimnt nq,nfcorpus,fiqa,scifact .' )
 parser.add_argument('-d','--dataset', type=str,help='dataset name',default='fiqa')
-dataset = "fiqa"
+dataset = parser.parse_args().dataset
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 out_dir = os.path.join(pathlib.Path(__file__).parent.absolute(), "./datasets")
 util.download_and_unzip(url, out_dir)
